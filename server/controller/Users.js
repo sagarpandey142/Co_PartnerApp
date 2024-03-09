5d15ca339642c31b5f17805a0982f6e48226c596
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-09 10:01:42 +0530

    github commit

--- server/controller/Users.js
+++ server/controller/Users.js
@@ -1,2 +1,26 @@
+const User=require("../Models/User")
 
+exports.signup=async(req,res)=>{
+   try{
+    const{Full_Name,Dob,Tech,Year,Email,GithubLink,LinkedinLink,password}=req.body;
+    if(!Full_Name || !Dob  || Tech || !Year || !Email || !GithubLink || !LinkedinLink || !password){
+        return res.status(400).json({
+            message:"All Field re Required",
+          }) 
+    }
+    if(User.find(Email)){
+        return res.status(400).json({
+            message:"Email Already Exists",
+          }) 
+    }
 
+    //hasing
+    
+
+   } catch(error){
+      return res.json({
+        message:"Error Occuried",
+        error:error
+      })
+   }
+}
