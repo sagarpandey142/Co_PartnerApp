d5a846e12afd98ed5f80e83c658b8e45c443ce63
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-10 15:35:26 +0530

    client commit

---
+++ Client/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MyComponent from './Component/Signup/Index.js';
+export default function App() {
+  return (
+    <View >
+      <MyComponent/>
+    </View>
+  );
+
+
+};
---
+++ Client/Component/Signup/Index.js
@@ -0,0 +1,13 @@
+// MyComponent.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+  const MyComponent = () => {
+  return (
+    <View>
+      <Text>Hello from MyComponent!</Text>
+    </View>
+  );
+};
+
+export default MyComponent;
