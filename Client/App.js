ac09f2e302fdfb7b620d76e154dd57cd3b7dd252
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-16 12:48:55 +0530

    get started commit

--- Client/App.js
+++ Client/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'twrnc'
+import GetStarted from './Component/GetStarted';
 
 export default function App() {
   return (
     <View style={tw`w-[100%] h-[100%]`}>
-      <Text style={tw` w-[100%] h-[100%] flex item-center justify-center`}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <GetStarted/>
     </View>
   );
 }
