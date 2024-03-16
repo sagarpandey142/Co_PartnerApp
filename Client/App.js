3fb9fde474e68523b6a61f530770387f985e2af4
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-16 09:46:57 +0530

    initial com

--- Client/App.js
+++ Client/App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`w-[100%] h-[100%]`}>
+      <Text style={tw` w-[100%] h-[100%] flex item-center justify-center`}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
