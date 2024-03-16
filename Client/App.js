e295f41d88573e07946b1553e1cee31a65fb961c
priyaK9157 <priyakumari2003004@gmail.com>
2024-03-16 12:50:22 +0530

    first commit

--- Client/App.js
+++ Client/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import tw from 'twrnc'
+import tw from 'twrnc';
+import SignUp from './Pages/SignUp';
+import { Provider } from 'react-redux';
+import store from './store'; // Import your Redux store
 
 export default function App() {
   return (
-    <View style={tw`w-[100%] h-[100%]`}>
-      <Text style={tw` w-[100%] h-[100%] flex item-center justify-center`}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}> {/* Wrap your component with Provider */}
+      <View style={tw`w-[100%] h-[100%]`}>
+        <Text style={tw` w-[100%] h-[100%] flex items-center justify-center`}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <SignUp />
+      </View>
+    </Provider>
   );
 }
-
-
