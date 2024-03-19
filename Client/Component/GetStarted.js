dda75a389dcb6c23162269fd8143e2c21a19b372
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-19 16:17:52 +0530

    footer

--- Client/Component/GetStarted.js
+++ Client/Component/GetStarted.js
@@ -8,7 +8,7 @@ import { useNavigation } from '@react-navigation/native';
 const GetStarted = () => {
   const navigation = useNavigation();
   const handlePress = () => {
-    navigation?.navigate('ProffesionalInfo');
+    navigation?.navigate('Signup');
   };
   return (
     <ImageBackground source={office} style={tw`flex-1`}>
