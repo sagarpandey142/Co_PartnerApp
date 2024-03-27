82ad8718822a85b7075ee512b0ba9d0aea5cad14
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-27 15:07:53 +0530

    project

--- Client/services/Api.js
+++ Client/services/Api.js
@@ -1,6 +1,6 @@
 exports.generateVerifyOTP = {
-    generateOTP : "http://192.168.79.20:4000/v1/getOtp",
-    verifyOTP : "http://192.168.79.20:4000/v1/verifyOtp",
+    generateOTP : "http://192.168.50.20:4000/v1/getOtp",
+    verifyOTP : "http://192.168.50.20:4000/v1/verifyOtp",
     login : "http://192.168.50.20:4000/v1/login",
     DecodedApi:"http://192.168.50.20:4000/v1/DecodToken"
 
